fe1d88097ad511fdc959dfbbd2aae8d3814fa7f0
AyushJ16 <95370097+AyushJ16@users.noreply.github.com>
2023-08-18 19:21:12 +0530

    Time: 19 ms (78.82%), Space: 16.2 MB (42.91%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,63 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-unordered_map<long,int>dis;
-    void dfs(TreeNode* root,int &t,int &ans,long &sum){
-        // sum+=root->val;
-        // dis[sum]=1;
-        if(sum==t){
-            ans++;
-        }
-        cout<<sum<<" "<<dis[sum-t]<<endl;
-        if(dis[sum-t]>0){
-            if((sum-t)==sum && dis[sum-t]>1){cout<<sum<<endl;
-            ans+=dis[sum-t]-1;}
-            else if(sum-t != sum)
-            ans+=dis[sum-t];
-        }
-        if(root->left){
-            sum+=root->left->val;
-            dis[sum]++;
-            dfs(root->left,t,ans,sum);
-            dis[sum]--;
-            sum-=root->left->val;
-        }
-        if(root->right){
-            sum+=root->right->val;
-            dis[sum]++;
-            // if(dis[sum-t]==1){
-            //     if(sum-t==sum && dis[sum]>1){cout<<sum<<endl;
-            //     ans++;}
-            //     else if(sum-t != sum)
-            //     ans++;
-            // }
-            dfs(root->right,t,ans,sum);
-            dis[sum]--;
-            sum-=root->right->val;
-        }
-        // dis[sum]=0;
-        // sum-=root->val;
-
-
-    }
 public:
-    int pathSum(TreeNode* root, int targetSum) {
-        if(!root){
-            return 0;
+    void solve(TreeNode* root,vector<int> &v,int &ct,int k){
+        if(root == NULL) return ;
+        v.push_back(root->val);
+
+        solve(root->left,v,ct,k);
+        solve(root->right,v,ct,k);
+
+        int n = v.size();
+        long long sum = 0;
+
+        for(int i = n - 1;i >= 0;--i){
+            sum += v[i];
+            if(sum == k)
+                ct++;
         }
-        long sum=root->val;
-        dis[sum]++;
-        int ans=0;
-        dfs(root,targetSum,ans,sum);
-        return ans;
+        v.pop_back();
+    }
+    int pathSum(TreeNode* root, int k) {
+        vector<int> v;
+        int ct = 0;
+        solve(root,v,ct,k);
+        return ct;
     }
 };
