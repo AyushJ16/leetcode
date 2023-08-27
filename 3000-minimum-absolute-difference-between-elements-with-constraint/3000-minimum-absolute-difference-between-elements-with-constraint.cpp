057d332362bcdef9491d4b3c243f3b28f786b082
AyushJ16 <95370097+AyushJ16@users.noreply.github.com>
2023-08-27 23:05:28 +0530

    Time: 998 ms (5.03%), Space: 240.5 MB (5.85%) - LeetHub

--- 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.cpp
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.cpp
@@ -1,29 +1,51 @@
 class Solution {
-public:
-    int minAbsoluteDifference(vector<int>& nums, int x) {
-        set<int> st;
-        int mini=INT_MAX;
-        for(int i=x;i<nums.size();i++){
+private:
+    bool check(vector<int>&nums,int m,int x){
+        set<int>st;
+        int n=nums.size();
+        for(int i=x;i<n;i++){
             st.insert(nums[i-x]);
             auto it=st.lower_bound(nums[i]);
             if(it==st.end()){
                 it--;
             }
-            mini=min(mini,abs(nums[i]-*(it)));
+            if(abs(nums[i]-*(it))<=m){
+                return true;
+            }
             if(st.find(nums[i])==st.end()){
                 if(it!=st.begin()){
                     it--;
                 }
-                mini=min(mini,abs(nums[i]-*(it)));
+                if(abs(nums[i]-*(it))<=m){
+                    return true;
+                }
             }
             else{
                 it++;
-                mini=min(mini,abs(nums[i]-*(it)));
+                return abs(nums[i]-*(it))<=m;
             }
 
-
+            
         }
-        return mini;
-        
+        return false;
+    }
+public:
+    int minAbsoluteDifference(vector<int>& nums, int x) {
+        int n=nums.size();
+        int mini=*min_element(nums.begin(),nums.end());
+        int maxi=*max_element(nums.begin(),nums.end());
+        int l=0,r=maxi-mini;
+        int res=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(check(nums,mid,x)){
+                res=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return res;
     }
 };
