c5a1027166ce23d7ba4fd324b259607155fa4d24
AyushJ16 <95370097+AyushJ16@users.noreply.github.com>
2023-08-27 18:54:13 +0530

    Time: 73 ms (95.92%), Space: 61.9 MB (49.46%) - LeetHub

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int helper(vector<int>& nums, int target,int index,vector<int>&dp){
         if(index==nums.size()-1) return 0;
-        int maxi=INT_MIN;
-        if(dp[index]!=-1) return dp[index];
+        int maxi=-1;
+        if(dp[index]!=0) return dp[index];
         for(int i=index+1;i<nums.size();i++){
             if(abs(nums[index]-nums[i])<=target){
                     int t=helper(nums,target,i,dp);
@@ -14,7 +14,7 @@ public:
         return dp[index]=maxi;
     }
     int maximumJumps(vector<int>& nums, int target) {
-        vector<int> dp(nums.size(),-1);
+        vector<int> dp(nums.size(),0);
         int ans=helper(nums,target,0,dp);
         return ans<=0?-1:ans;
     }
