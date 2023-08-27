472bdbbfd1797ffbf0d457e38d9267d37259e7f4
AyushJ16 <95370097+AyushJ16@users.noreply.github.com>
2023-08-27 18:24:54 +0530

    Time: 146 ms (61.69%), Space: 40.8 MB (56.9%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-    bool canCross(vector<int>& stones) {
-        int n=stones.size();
-        vector<vector<int>> pos(n);
-        if(n==1 || (n==2 && stones[1]==1)){
-            return true;
+    bool canCross(std::vector<int>& stones) {
+        int n = stones.size();
+
+        std::unordered_map<int, std::unordered_set<int>> dp;
+        for (int stone : stones) {
+            dp[stone] = std::unordered_set<int>();
         }
-        if(stones[1]!=1){
-            return false;
-        }
-        pos[1].push_back(1);
-        pos[1].push_back(2);
-        bool f=false;
-        for(int i=2;i<n;i++){
-            f=false;
-            for(int j=1;j<i;j++){
-                // if((stones[j]-stones[i]) > stones[j]+1){
-                //     continue;
-                // }
-                    if(stones[i]-stones[j]  > stones[j]+1){
-                        continue;
-                    }
-                for(auto &jumps:pos[j]){
-                    if(jumps+stones[j]==stones[i]){
-                        cout<<i<<" "<<j<<" "<<jumps<<endl;
-                        pos[i].push_back(stones[i]-stones[j]);
-                        pos[i].push_back(stones[i]-stones[j]-1);
-                        pos[i].push_back(stones[i]-stones[j]+1);
-                        f=true;
-                        break;
+        dp[0].insert(0);
+
+        for (int i = 0; i < n; i++) {
+            for (int k : dp[stones[i]]) {
+                for (int step = k - 1; step <= k + 1; step++) {
+                    if (step != 0 && dp.count(stones[i] + step)) {
+                        dp[stones[i] + step].insert(step);
                     }
                 }
             }
-        }if(pos[n-1].size()>0)
-        return true;
-        return false;
-
+        }
+        
+        return !dp[stones.back()].empty();
     }
 };
