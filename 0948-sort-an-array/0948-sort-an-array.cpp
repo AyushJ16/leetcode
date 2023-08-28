1606a021ef6a3e8dafecfb17612ef42a13e58a90
AyushJ16 <95370097+AyushJ16@users.noreply.github.com>
2023-08-28 17:49:21 +0530

    Time: 78 ms (99.7%), Space: 76.1 MB (48.77%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,48 +1,17 @@
 class Solution {
-private:
-    void merge(int l,int r,int mid,vector<int>&nums){
-        vector<int> temp;
-        int i=l;
-        int k=mid;
-        int j=mid+1;
-        while(i<=k && j<=r){
-            if(nums[i]>nums[j]){
-                temp.push_back(nums[j]);
-                j++;
-            }
-            else{
-                temp.push_back(nums[i]);
-                i++;
-            }
-        }
-        while(i<=k){
-            temp.push_back(nums[i]);
-            i++;
-        }
-        while(j<=r){
-            temp.push_back(nums[j]);
-            j++;
-        }
-        for(int s=l;s<=r;s++){
-            nums[s]=temp[s-l];
-        }
-        return;
-    }
-    void sort(int l,int r,vector<int> &nums){
-        if(l>=r){
-            return;
-        }
-        int mid=(l+r)/2;
-        sort(l,mid,nums);
-        sort(mid+1,r,nums);
-        merge(l,r,mid,nums);
-    }
-
 public:
     vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size();
-        int l=0,r=n-1;
-        sort(l,r,nums);
+        vector<int>count(100001,0);
+        for(auto &e:nums){
+            count[e+50000]++;
+        }
+        int pos=0;
+        for(int i=0;i<=100000;i++){
+            for(int j=0;j<count[i];j++){
+                nums[pos]=i-50000;
+                pos++;
+            }
+        }
         return nums;
     }
 };
