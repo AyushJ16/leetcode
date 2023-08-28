0af8e600a0ecdcf85694a88025011b0004ac8b64
AyushJ16 <95370097+AyushJ16@users.noreply.github.com>
2023-08-28 18:14:22 +0530

    Time: 142 ms (87.59%), Space: 78 MB (48.58%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,17 +1,39 @@
 class Solution {
+private:
+    void countSort(vector<int>&nums,int n,int exp){
+        vector<int> output(n);
+        vector<int>count(10,0);
+        for(int i=0;i<n;i++){
+            count[(nums[i]/exp)%10]++;
+        }
+        for(int i=1;i<=9;i++){
+            count[i]+=count[i-1];
+        }
+        for(int i=n-1;i>=0;i--){
+            output[count[(nums[i]/exp)%10 ]-1]=nums[i];
+            count[(nums[i]/exp)%10]--;
+        }
+        for(int i=n-1;i>=0;i--){
+            nums[i]=output[i];
+        }
+    }
+    void radixsort(vector<int> &nums,int n){
+        int m=*max_element(nums.begin(),nums.end());
+        for(int exp=1;m/exp>0;exp*=10){
+            countSort(nums,n,exp);
+        }
+    }
 public:
     vector<int> sortArray(vector<int>& nums) {
-        vector<int>count(100001,0);
-        for(auto &e:nums){
-            count[e+50000]++;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            nums[i]+=50000;
         }
-        int pos=0;
-        for(int i=0;i<=100000;i++){
-            for(int j=0;j<count[i];j++){
-                nums[pos]=i-50000;
-                pos++;
-            }
+        radixsort(nums,n);
+        for(int i=0;i<n;i++){
+            nums[i]-=50000;
         }
         return nums;
+
     }
 };
