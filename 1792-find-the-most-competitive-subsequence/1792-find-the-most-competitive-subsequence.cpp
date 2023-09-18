dd5dc5d44517b4eea369146018c36e7da52ffcd6
AyushJ16 <95370097+AyushJ16@users.noreply.github.com>
2023-09-18 20:32:17 +0530

    Time: 201 ms (17.63%), Space: 113.7 MB (14.79%) - LeetHub

---
+++ 1792-find-the-most-competitive-subsequence/1792-find-the-most-competitive-subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        int n=nums.size();
+        stack<int>st;
+        int mp=n-k;
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                st.push(nums[i]);
+            }
+            else{
+                while(!st.empty() && st.top()>nums[i] && mp>0 && st.size()+n-i-1 >= (k)){
+                    if(st.size()+n-i-1 < (k)){
+                        break;
+                    }
+                    mp--;
+                    st.pop();
+                }
+                st.push(nums[i]);
+            }
+        }
+        cout<<st.size()<<endl;
+        while(mp--){
+            cout<<st.top()<<" ";
+            st.pop();
+        }
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
